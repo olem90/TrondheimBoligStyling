a682f784724d7bfcf6f7023cdabde3c590b8c9d4
olem90 <olemariusrognan90@gmail.com>
2024-10-17 17:34:39 +0100

    testing

--- src/components/Layout/Layout.style.jsx
+++ src/components/Layout/Layout.style.jsx
@@ -1,10 +1,6 @@
 import styled from "styled-components";
 
 export const LayoutStyles = styled.div`
-  height: 100vh;
   min-width: 100%;
-
-  @media(max-width: 600px) {
-    height: 100%;
-  }
+  border: 1px solid transparent;
 `;
